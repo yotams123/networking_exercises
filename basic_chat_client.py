7b1bf74220cdfc6eeeb6007aaa6ce18b21126df6
Yotam <yotamsoudry@gmail.com>
2022-07-10 12:10:23 +0300

    a

--- basic_chat_client.py
+++ basic_chat_client.py
@@ -1,13 +1,17 @@
 import socket
+import time
+
+start_time = time.time()
 
 my_socket = socket.socket()
-
 my_socket.connect(('127.0.0.1', 8820))
 
-msg = input("Enter Message: ")
-my_socket.send(msg.encode())
+my_socket.send("Yotam".encode())
 
 data = my_socket.recv(1024).decode()
 print("The server sent: " + data)
 
 my_socket.close()
+
+end_time = time.time()
+print(f"----{end_time - start_time} secs ----")
--- ch6/basic_udp_client.py
+++ ch6/basic_udp_client.py
@@ -1,8 +1,13 @@
 import socket
+import time
 
+start_time = time.time()
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # udp server socket
 my_socket.sendto("Yotam".encode(), ('127.0.0.1', 8821))
 
 (data, client) = my_socket.recvfrom(1024)
 print('The server sent: ' + data.decode())
 my_socket.close()
+
+end_time = time.time()
+print(f"----{end_time - start_time} secs----")
