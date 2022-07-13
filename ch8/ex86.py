c67c6ff69564f8b64ccfe037d4717a0b3ea4f04b
Yotam <yotamsoudry@gmail.com>
2022-07-13 13:18:23 +0300

    exercise 86

---
+++ ch8/ex86.py
@@ -0,0 +1,25 @@
+addr = input("Enter mac address with each byte separated by a colon 'AA:BB:CC:DD:EE:FF' > ")
+
+VALID_CHARS = 'ABCDEF1234567890'
+MULTICAST = '13579BDF'
+
+b = addr.split(":")
+
+if len(b) == 6:
+    flag = True
+else:
+    flag = False
+
+for byte in b:
+    if len(byte) != 2 or byte[0].upper() not in VALID_CHARS or byte[1].upper() not in VALID_CHARS:
+        flag = False
+
+if flag:
+    print("Valid MAC address")
+    print(f"Vendor ID is {':'.join(b[:3])}")
+    if b[0][1] in MULTICAST:
+        print("MAC  address is a multicast address")
+    else:
+        print("MAC address is a unicast address")
+else:
+    print("Invalid MAC address")
