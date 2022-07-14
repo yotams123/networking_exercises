b3f75905017b142460b2ba68088c9328ac0d1d3f
Yotam <yotamsoudry@gmail.com>
2022-07-14 13:54:03 +0300

    exercises 12-5,6

--- ch12/ex12-5/chat_client.py
+++ ch12/ex12-5/chat_client.py
@@ -6,30 +6,64 @@ SERVER_IP = '127.0.0.1'
 SERVER_PORT = 5555
 MAX_LENGTH = 1024
 
+USERNAME = input("Enter username: ")
+while USERNAME[0] == '@':
+    USERNAME = input("Enter username: ")
+
 client_socket = socket.socket()
 client_socket.connect((SERVER_IP, SERVER_PORT))
-
-
+client_socket.send(f"{str(len(USERNAME)).zfill(2)}{USERNAME}".encode())
 msg = ""
+
+
+def protocol(message):
+    print()
+    while True:
+        cmd = input("Enter type of command, between 1 and 5> ")
+        try:
+            if int(cmd) <= 5:
+                break
+        except ValueError:
+            continue
+
+    if cmd == '1':
+        print()
+        return f"{str(len(USERNAME)).zfill(2)}{USERNAME}{cmd}{str(len(message)).zfill(4)}{msg}"
+
+    if cmd == '2':
+        appointed = input("Enter user to appoint: ")
+        return f"{str(len(USERNAME)).zfill(2)}{USERNAME}{cmd}{str(len(appointed)).zfill(2)}{appointed}"
+
+    if cmd == '3':
+        kicked = input("Enter user to kick: ")
+        return f"{str(len(USERNAME)).zfill(2)}{USERNAME}{cmd}{str(len(kicked)).zfill(2)}{kicked}"
+
+    if cmd == '4':
+        mute = input("Enter user to mute/unmute: ")
+        return f"{str(len(USERNAME)).zfill(2)}{USERNAME}{cmd}{str(len(mute)).zfill(2)}{mute}"
+
+    if cmd == '5':
+        rec = input("Enter recipient of private message: ")
+        return f"{str(len(USERNAME)).zfill(2)}{USERNAME}{cmd}{str(len(rec)).zfill(2)}{rec}{str(len(msg)).zfill(4)}{msg}"
+
+
 while True:
     rlist, wlist, xlist = select.select([client_socket], [client_socket], [])
     if msvcrt.kbhit():
         char = msvcrt.getch()
         if char == b'\r':
             if client_socket in wlist:
-                if msg == "":
+                client_socket.send(protocol(msg).encode())
+                if msg == "quit":
                     client_socket.close()
                     break
-                else:
-                    client_socket.send(msg.encode())
-                    msg = ""
+                msg = ""
                 print()
         else:
-            if len(msg) == 0:
-                print("You:\t", end='')
             char = char.decode()
             print(char, end='')
             msg += char
     if client_socket in rlist:
-        data = client_socket.recv(MAX_LENGTH).decode()
+        data_length = client_socket.recv(4).decode()
+        data = client_socket.recv(int(data_length)).decode()
         print(data)
--- ch12/ex12-5/chat_server.py
+++ ch12/ex12-5/chat_server.py
@@ -1,5 +1,6 @@
 import socket
 import select
+import datetime
 
 MAX_LENGTH = 1024
 SERVER_IP = '0.0.0.0'
@@ -11,30 +12,103 @@ server_socket.listen()
 
 open_clients = []
 pending_msgs = []
+admins = ['ysman']
 
+clients = []
 while True:
     rlist, wlist, xlist = select.select([server_socket] + open_clients, open_clients, [])
     for client in rlist:
         if client is server_socket:
             (connection, addr) = server_socket.accept()
-            print(f"New client {addr} connected!")
+            uname_length = connection.recv(2).decode()
+            uname = connection.recv(int(uname_length)).decode()
             open_clients.append(connection)
+            clients.append([connection, uname, False])
+            pending_msgs.append((f"New client {uname} connected! ", open_clients.copy()))
         else:
-            data = client.recv(MAX_LENGTH).decode()
-            if data == "":
-                open_clients.remove(client)
-                client.close()
-                pending_msgs.append((f"Client {client.getpeername()} disconnected", open_clients))
-                print(f"Client {client.getpeername()} disconnected")
-            else:
-                print("Data received: ", data)
-                p = open_clients.copy()
-                p.remove(client)
-                pending_msgs.append((data, p))
+            uname_length = client.recv(2).decode()
+            uname = client.recv(int(uname_length)).decode()
+            cmd = client.recv(1).decode()
+            time = datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')
+
+            if cmd == '1':
+                msg_length = client.recv(4).decode()
+                data = client.recv(int(msg_length)).decode()
+                if data != "quit" and data != 'view admins':
+                    for i in clients:
+                        if i[1] == uname and not i[2]:
+                            if uname in admins:
+                                pending_msgs.append((f"{time}\t@{uname}:\t{data}", open_clients.copy()))
+                            else:
+                                pending_msgs.append((f"{time}\t{uname}:\t{data}", open_clients.copy()))
+                        elif i[1] == uname:
+                            pending_msgs.append((f"You cannot speak here", [client]))
+                elif data == 'view admins':
+                    msg = admins[0]
+                    for a in admins[1:]:
+                        msg += ", " + a
+                    pending_msgs.append((msg, [client]))
+                else:
+                    open_clients.remove(client)
+                    client.close()
+                    pending_msgs.append((f"{time}\t{uname} disconnected", open_clients.copy()))
+
+            if cmd == '2':
+                if uname not in admins:
+                    pending_msgs.append(("Must be an admin to use this functionality", [client]))
+                    client.recv(MAX_LENGTH)
+                    continue
+                appointed_length = client.recv(2).decode()
+                appointed = client.recv(int(appointed_length)).decode()
+                admins.append(appointed)
+                rec = None
+                for i in clients:
+                    if i[1] == appointed:
+                        rec = i[0]
+                pending_msgs.append(("You are now an admin!", [rec]))
+            if cmd == '3':
+                if uname not in admins:
+                    pending_msgs.append(("Must be an admin to use this functionality", [client]))
+                    client.recv(MAX_LENGTH)
+                    continue
+                kicked_length = client.recv(2).decode()
+                kicked = client.recv(int(kicked_length)).decode()
+                for i in clients.copy():
+                    if i[1] == kicked:
+                        open_clients.remove(i[0])
+                        i[0].close()
+                        pending_msgs.append((f"{time}\t{i[1]} was kicked by {uname}", open_clients.copy()))
+                        clients.remove(i)
+
+            if cmd == '4':
+                if uname not in admins:
+                    pending_msgs.append(("Must be an admin to use this functionality", [client]))
+                    client.recv(MAX_LENGTH)
+                    continue
+                muted_length = client.recv(2).decode()
+                muted = client.recv(int(muted_length)).decode()
+                for i in clients.copy():
+                    if i[1] == muted:
+                        i[2] = not i[2]
+                        if i[2]:
+                            pending_msgs.append(("You are now mute", [i[0]]))
+                        else:
+                            pending_msgs.append(("You are now unmute", [i[0]]))
+            if cmd == '5':
+                rec_len = client.recv(2).decode()
+                rec = client.recv(int(rec_len)).decode()
+
+                msg_len = client.recv(4).decode()
+                msg = client.recv(int(msg_len)).decode()
+
+                for i in clients:
+                    if i[1] == rec:
+                        pending_msgs.append((f"{time}\t!{uname}:\t{msg}", [client, i[0]]))
+
     for data, receivers in pending_msgs:
         for receiver in receivers:
             if receiver in wlist:
-                receiver.send(f"{receiver.getpeername()}:\t{data}".encode())
+                receiver.send(f"{str(len(data)).zfill(4)}{data}".encode())
                 receivers.remove(receiver)
         if not receivers:
             pending_msgs.remove((data, receivers))
